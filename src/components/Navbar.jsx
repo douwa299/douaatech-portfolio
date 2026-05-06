203689313ce58691dd3e64986f3b367c5dd99e19
douwa299 <douaaberrahmo@gmail.com>
2026-05-06 14:58:06 +0100

    Added My CV

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,6 +31,14 @@ export const Navbar = ( {menuOpen,setMenuOpen}) => {
             <a href="#about" className="text-gray-300 hover:text-white transition-colors">About</a>
             <a href="#projects" className="text-gray-300 hover:text-white transition-colors">Projects</a>
             <a href="#contact" className="text-gray-300 hover:text-white transition-colors">Contact</a>
+            <a
+            href="/douaatech-portfolio/RealCv.pdf"
+            download
+            className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5
+                 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+          >
+            Download my CV
+          </a>
           </div>
         </div>
       </div>
