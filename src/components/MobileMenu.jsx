e9b9c3c78c33a15c4274717dceaec00dfd467f32
douwa299 <douaaberrahmo@gmail.com>
2026-05-12 14:47:35 +0100

    Check for visitor

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -52,7 +52,7 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
         Contact
       </a>
       <a
-        href="/douaatech-portfolio/Douaa_Berrahmo_CV.pdf"
+        href="/douaatech-portfolio/BerrahmoDouaa_CV.pdf"
         download
         className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5
                  hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
